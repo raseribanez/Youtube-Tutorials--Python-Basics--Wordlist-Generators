78ab720c2bbde7cb88dc63ef32c5b66e22d023a9
Ben Woodfield <raser.13@hotmail.com>
2016-11-04 21:02:04 +0000

    added comments to tutorial code

--- wordlist_basic.py
+++ wordlist_basic.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env/python
+# Ben Woodfield
 import itertools
 
-chrs = 'abcd'
-n = 5
+chrs = 'abcd' # Change your required characters here
+n = 5 # Change your word length here
 
 for xs in itertools.product(chrs, repeat=n):
    print ''.join(xs)
